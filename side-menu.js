7ff928e5a9ab45caf52276fc59c96e329432fbd6
bpololo <pololo6644@gmail.com>
2026-01-15 00:34:44 +0300

    Update side-menu.js

--- side-menu.js
+++ side-menu.js
@@ -3,9 +3,10 @@ document.write(' \
       <div id="menu"> \
         <div class="pure-menu"> \
             <ul class="pure-menu-list"> \
-                <li class="pure-menu-heading">2023-24</li> \
+                <li class="pure-menu-heading">All</li> \
                 <li class="pure-menu-item"><a class="pure-menu-link" href="attendance.html">Посещаемость</a></li> \
-                <li class="pure-menu-item"><a class="pure-menu-link" href="sbp_vs_bpm_attendance.html">Сравнение</a></li> \
+                <li class="pure-menu-item"><a class="pure-menu-link" href="sbp_vs_bpm_attendance.html">SBP vs BPM</a></li> \
+                <li class="pure-menu-heading">2023-24</li> \
                 <li class="pure-menu-item"><a class="pure-menu-link" href="first_question.html">1-й вопрос 23-24</a></li> \
                 <li class="pure-menu-item"><a class="pure-menu-link" href="first_question_24.html">1-й вопрос 24</a></li> \
                 <li class="pure-menu-heading">2025</li> \
