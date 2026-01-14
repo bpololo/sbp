0cf92e2a3cbd7338bd7e644f543d2acdbfe48d1b
bpololo <pololo6644@gmail.com>
2026-01-14 23:11:22 +0300

    Create side-menu.js

---
+++ side-menu.js
@@ -0,0 +1,19 @@
+document.write('
+     <div id="menu"> \
+        <div class="pure-menu"> \
+            <ul class="pure-menu-list"> \
+                <li class="pure-menu-heading">2023-24</li>
+                <li class="pure-menu-item"><a class="pure-menu-link" href="attendance.html">Посещаемость</a></li> \
+                <li class="pure-menu-item"><a class="pure-menu-link" href="sbp_vs_bpm_attendance.html">Сравнение</a></li> \
+                <li class="pure-menu-item"><a class="pure-menu-link" href="first_question.html">Первый вопрос 23-24</a></li> \
+                <li class="pure-menu-item"><a class="pure-menu-link" href="first_question_24.html">Первый вопрос 24</a></li> \
+                <li class="pure-menu-item"><a class="pure-menu-link" href="second_question.html">Второй вопрос 24</a></li> \
+                <li class="pure-menu-item"><a class="pure-menu-link" href="third_question.html">Третий вопрос 24</a></li> \
+                <li class="pure-menu-heading">2025</li> \
+                <li><a href="first_question_25.html">Первый вопрос 25</a></li> \
+                <li><a href="second_question_25.html">Второй вопрос 25</a></li> \
+                <li><a href="third_question_25.html">Третий вопрос 25</a></li> \
+            </ul>
+        </div>
+    </div>
+');
