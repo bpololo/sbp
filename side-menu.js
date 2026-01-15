498e8ab1c155cb2ef0215542f9f5bb200b4cc69f
bpololo <pololo6644@gmail.com>
2026-01-15 17:42:38 +0300

    Update side-menu.js

--- side-menu.js
+++ side-menu.js
@@ -6,11 +6,17 @@ document.write(' \
                 <li class="pure-menu-heading">凸(￣ヘ￣)</li> \
                 <li class="pure-menu-item"><a class="pure-menu-link" href="attendance.html">Посещаемость</a></li> \
                 <li class="pure-menu-item"><a class="pure-menu-link" href="sbp_vs_bpm_attendance.html">SBP vs BPM</a></li> \
-                <li class="pure-menu-heading">2023-24</li> \
+                <li class="pure-menu-heading">SBP 2023-24</li> \
                 <li class="pure-menu-item"><a class="pure-menu-link" href="first_question.html">1-й вопрос 23-24</a></li> \
                 <li class="pure-menu-item"><a class="pure-menu-link" href="first_question_24.html">1-й вопрос 24</a></li> \
-                <li class="pure-menu-heading">2025</li> \
+                <li class="pure-menu-heading">SBP 2025</li> \
                 <li class="pure-menu-item"><a class="pure-menu-link" href="first_question_25.html">1-й вопрос</a></li> \
+                <li class="pure-menu-item"><a class="pure-menu-link" href="second_question_25.html">2-й вопрос</a></li> \
+                <li class="pure-menu-item"><a class="pure-menu-link" href="third_question_25.html">3-й вопрос</a></li> \
+            </ul> \
+            <ul class="pure-menu-list"> \
+                <li class="pure-menu-heading" style="background:#ff00cd">BPM 2025</li> \
+                <li class="pure-menu-item"><a class="pure-menu-link" href="first_question_msk25.html">1-й вопрос</a></li> \
                 <li class="pure-menu-item"><a class="pure-menu-link" href="second_question_25.html">2-й вопрос</a></li> \
                 <li class="pure-menu-item"><a class="pure-menu-link" href="third_question_25.html">3-й вопрос</a></li> \
             </ul> \
